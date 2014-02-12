1101caceb4f2e64756f700d76681fe5971e04b4e
Jeff Kilroy <jeffkilroy@gmail.com>
2014-02-12 14:40:28 -0500

    init

---
+++ index.php
@@ -0,0 +1,25 @@
+
+<!doctype html>
+
+<!--[if lt IE 7]><html lang="en-US" class="no-js lt-ie9 lt-ie8 lt-ie7"><![endif]-->
+<!--[if (IE 7)&!(IEMobile)]><html lang="en-US" class="no-js lt-ie9 lt-ie8"><![endif]-->
+<!--[if (IE 8)&!(IEMobile)]><html lang="en-US" class="no-js lt-ie9"><![endif]-->
+<!--[if gt IE 8]><!--> <html lang="en-US" class="no-js"><!--<![endif]-->
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+	<title>Hello</title>
+
+	<meta name="HandheldFriendly" content="True">
+	<meta name="MobileOptimized" content="320">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+</head>
+
+<body class="home page page-id-4 page-template page-template-page-home-php logged-in admin-bar no-customize-support">
+	<div id="page">
+hello
+	</div><!--/page-->
+		</body>
+</html>
